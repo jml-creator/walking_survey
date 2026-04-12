74216067accac1122e6e56a75b42697c2709dbed
Jae Min Lee <jaeminlee@Jaes-MacBook-Pro.local>
2026-04-12 20:09:12 +0900

    fix: isDbxConnected length check + bust SW cache v2

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = 'survey-v1';
+const CACHE = 'survey-v2';
 const URLS = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS))); });
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
